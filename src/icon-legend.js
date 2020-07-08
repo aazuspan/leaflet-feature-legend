de3f0a73decae7196753326f9357151dec5fa064
azuspan607 <aa.zuspan@gmail.com>
2020-07-08 12:35:41 -0700

    Split container building into separate methods, and use DomUtil instead of string literals

--- src/icon-legend.js
+++ src/icon-legend.js
@@ -7,36 +7,38 @@ L.Control.IconLegend = L.Control.extend({
 
     initialize: function (options) {
         L.Util.setOptions(this, options);
+        this._buildContainer();
+    },
+
+    _buildContainer: function () {
+        this._container = L.DomUtil.create('div', 'leaflet-control-legend');
+
+        this._buildTitle();
+        this._buildItems();
+    },
+
+    _buildTitle: function () {
+        if (this.options.title) {
+            let title = L.DomUtil.create('h3', 'leaflet-control-legend-title', this._container);
+            title.innerHTML = this.options.title;
+        }
+    },
+
+    _buildItems: function () {
+        if (this.options.items) {
+            for (let item in this.options.items) {
+                let itemDiv = L.DomUtil.create('div', '', this._container);
+
+                let itemIcon = L.DomUtil.create('img', 'leaflet-control-legend-icon', itemDiv);
+                itemIcon.src = this.options.items[item].options.iconUrl;
+
+                let itemTitle = L.DomUtil.create('span', '', itemDiv);
+                itemTitle.innerHTML = item;
+            }
+        }
     },
 
     onAdd: function () {
-        this._container = L.DomUtil.create('div', 'leaflet-control-legend');
-
-        let title = this.options.title ?
-            `
-            <h4 class="leaflet-control-legend-title">
-                ${this.options.title}
-            </h4>
-            `
-            :
-            "";
-
-        let items = ""
-
-        for (let item in this.options.items) {
-            let itemElement = `
-                <div>
-                    <img src="${this.options.items[item].options.iconUrl}" class="leaflet-control-legend-icon" style='background:rgb(68, 140, 203);'></img>
-                    <span>${item}</span>
-                </div>
-            `
-            items += itemElement;
-        }
-
-        this._container.innerHTML = `
-            ${title}
-            ${items}
-            `;
         return this._container;
     },
 
